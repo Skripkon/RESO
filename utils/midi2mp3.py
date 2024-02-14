9ef671f6b550dd2a7e875e1557a1e8758057ab96
Fedchin Sergey <serg_fedchn@Sergey_Laptop.localdomain>
2024-02-14 23:01:19 +0300

    🔧 Fix 'file not found' error

--- utils/midi2mp3.py
+++ utils/midi2mp3.py
@@ -3,18 +3,22 @@ import os
 
 
 def midi2mp3(file_name):
+    """
+    Converts a specified .mid file into an .mp3 file and puts it into the same directory
+    """
     try:
         wav_file = os.path.abspath(file_name.replace('.mid', '.wav'))
-        mp3_file = os.path.abspath(file_name.replace('.mid', '.mp3'))
-        # print("saving wav", wav_file)
-
-        os.system(f'fluidsynth -ni utils/piano_soundfont.sf2 {file_name} -F {wav_file} -r 44100')
-    
+        file_path = os.path.abspath(file_name)
+        soundfont = os.path.abspath('utils/piano_soundfont.sf2')
+        # Run the conversion from .mid to .wav
+        os.system(
+            f'fluidsynth -ni "{soundfont}" "{file_path}" -F "{wav_file}" -r 44100')
+        # Convert to .mp3 from .wav
         audio = AudioSegment.from_wav(wav_file)
+        mp3_file = os.path.abspath(file_name.replace('.mid', '.mp3'))
         audio.export(mp3_file, format='mp3')
-
         os.remove(wav_file)
-        # print("success", wav_file, mp3_file)
+
         return mp3_file
 
     except Exception as e:
