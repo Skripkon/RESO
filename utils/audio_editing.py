eed054edaa63b0a21d2cfb7a2b53e7abcc569454
Fedchin Sergey <serg_fedchn@Sergey_Laptop.localdomain>
2024-02-27 20:04:51 +0300

    🔧 Fix function signature

--- utils/audio_editing.py
+++ utils/audio_editing.py
@@ -3,13 +3,19 @@ from pydub.generators import Sine
 import os
 
 
+def str_to_secs(time_str):
+    minutes, seconds = time_str.split(':')
+    return int(minutes) *  60 + int(seconds)
+
+
 def edit_mp3(relative_file_path: str, start_time: int, end_time: int, fade_in_len=0, fade_out_len=0):
     """
         Cuts an mp3 file according to given time and applies fade-in and fade-out. 
         Receieves a path to original mp3, start and end times in seconds, fade-in 
         and fade-out length in seconds. Resulting file is saved to the same folder 
         with prefix 'edited_' in mp3 format. If you do not need fade-in or fade-out, 
-        do not specify fade_in_len or fade_out_len.
+        you do not have to specify fade_in_len or fade_out_len. Returns edited file 
+        name.
     """
     file_path = os.path.abspath(relative_file_path)
     full_audio = AudioSegment.from_mp3(file_path)
@@ -26,6 +32,7 @@ def edit_mp3(relative_file_path: str, start_time: int, end_time: int, fade_in_le
     export_path = os.path.join(os.path.dirname(
         file_path), "edited_" + os.path.basename(file_path))
     cut_audio.export(export_path, format="mp3")
+    return "edited_" + os.path.basename(file_path)
 
 
 if __name__ == "__main__":
