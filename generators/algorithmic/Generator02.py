f65540d6e8011884ad165e89722e5b614f318ec4
Fedchin Sergey <serg_fedchn@Sergey_Laptop.localdomain>
2024-02-29 13:14:38 +0300

    💅 Style + naming

--- generators/algorithmic/Generator02.py
+++ generators/algorithmic/Generator02.py
@@ -5,27 +5,29 @@ import random
 tempo_map = {'Normal': 100, 'Slow': 60, 'Fast': 120}
 
 # it indicates the last chord for the left hand, so we can make sequences unique
-last_chord_for_the_left_hand_index = 0
-index_of_the_last_note_for_the_right_hand = 0
+left_hand_last_chord_index = 0
+right_hand_last_note_index = 0
 
 
-def generate_music02(scale: int, filepath: str, pulse: str = 'Normal', duration_sec: int = 60):
-    LENGTH_OF_THE_INTERVAl = 3
+def generate_music02(scale: int, filepath: str, tempo: str = 'Normal', duration_sec: int = 60):
+    INTERVAL_LENGTH = 3
+    OCTAVE_SHIFT = 12
     # Initialize music generator
     new_song_generator = MinorMusicGenerator(scale)
     # + new_song_generator.additional_chords
     chords = new_song_generator.minor_chords
 
-    notes_for_the_right_hand = new_song_generator.correct_notes.copy()
+    right_hand_notes = new_song_generator.correct_notes.copy()
 
-    for i in range(7):  # add notes from the 5th octave
-        notes_for_the_right_hand.append(notes_for_the_right_hand[i] + 12)
+    # Add notes from the 5th octave
+    for i in range(7):  
+        right_hand_notes.append(right_hand_notes[i] + OCTAVE_SHIFT)
 
-    # list of possible notes for the right hand
-    NUMBER_OF_NOTEX = len(notes_for_the_right_hand)
+    # List of possible notes for the right hand
+    CORRECT_NOTES_COUNT = len(right_hand_notes)
 
     # Get BPM
-    bpm = tempo_map.get(pulse, tempo_map['Normal'])
+    bpm = tempo_map.get(tempo, tempo_map['Normal'])
 
     # Initialize parts
     right_hand = stream.Part()
@@ -36,25 +38,25 @@ def generate_music02(scale: int, filepath: str, pulse: str = 'Normal', duration_
     left_hand.insert(0, tempo.MetronomeMark(number=bpm))
 
     # Calculate number of quarters
-    number_of_quarters = duration_sec * (bpm / 60)
+    quarters_count = duration_sec * (bpm / 60)
     durations = [3, 1.5, 1, 0.5]
 
     def add_one_interval():
-        global index_of_the_last_note_for_the_right_hand
-        # choose a duration of each note for the current interval (only for the right hand)
+        global right_hand_last_note_index
+        # Choose a duration of each note for the current interval (only for the right hand)
         random_duration_index = random.randint(0, len(durations) - 1)
         current_duration = durations[random_duration_index]
 
         velocity = random.randint(80, 110)
         # Generate notes for the right hand
-        number_of_notes = int(LENGTH_OF_THE_INTERVAl / current_duration)
-        for i in range(number_of_notes):
+        notes_count = int(INTERVAL_LENGTH / current_duration)
+        for i in range(notes_count):
             if (random.randint(1, 11) % 7 == 0):  # drop notes in a haphazard way
                 continue
 
             if (random.randint(1, 13) % 11 == 0):  # break the interval
                 # Insert a pause of specified duration
-                pause_duration = LENGTH_OF_THE_INTERVAl - i * current_duration
+                pause_duration = INTERVAL_LENGTH - i * current_duration
                 pause_note = note.Rest()
                 pause_note.duration.quarterLength = pause_duration
                 right_hand.append(pause_note)
@@ -70,43 +72,44 @@ def generate_music02(scale: int, filepath: str, pulse: str = 'Normal', duration_
                 continue
 
             random_delta_index = random.randint(-1, 1)
-            index_of_the_current_note = (index_of_the_last_note_for_the_right_hand +
-                                         random_delta_index) % NUMBER_OF_NOTEX
-            index_of_the_last_note_for_the_right_hand = index_of_the_current_note
-            random_note = notes_for_the_right_hand[index_of_the_current_note]
+            current_note_index = (
+                right_hand_last_note_index + random_delta_index) % CORRECT_NOTES_COUNT
+            right_hand_last_note_index = current_note_index
+            random_note = right_hand_notes[current_note_index]
             my_note = note.Note(random_note, quarterLength=current_duration)
             my_note.volume.velocity = velocity
             right_hand.append(my_note)
 
-        def add_a_chord_to_the_left_hand(idx: int, chord_shift: int, chord_velocity):
+        def add_chord_left_hand(idx: int, chord_shift: int, chord_velocity):
             random_chord = chords[idx].copy()
             random_chord.pop(random.randint(0, len(random_chord) - 1))
             for i in range(len(random_chord)):
-                random_chord[i] -= 12 * chord_shift
+                random_chord[i] -= OCTAVE_SHIFT * chord_shift
             newChord = chord.Chord(random_chord, quarterLength=1)
             newChord.volume.velocity = chord_velocity
             left_hand.append(newChord)
 
-        global last_chord_for_the_left_hand_index
+        global left_hand_last_chord_index
 
         # Choose a chord for the left hand
         random_index = random.randint(0, len(chords) - 1)
-        while (random_index == last_chord_for_the_left_hand_index):
+        while (random_index == left_hand_last_chord_index):
             random_index = random.randint(0, len(chords) - 1)
-        last_chord_for_the_left_hand_index = random_index
+        left_hand_last_chord_index = random_index
 
         # Add a chord that contains two notes, which are the same but at
         # neighbouring octaves (for the left hand)
-        random_chord = [chords[random_index][0] - 12, chords[random_index][0]]
+        random_chord = [chords[random_index][0] -
+                        OCTAVE_SHIFT, chords[random_index][0]]
         newChord = chord.Chord(random_chord, quarterLength=1)
         newChord.volume.velocity = 128
         left_hand.append(newChord)
         # Add two more chords for the left hand
-        add_a_chord_to_the_left_hand(random_index, 0, 85)
-        add_a_chord_to_the_left_hand(random_index, 0, 115)
+        add_chord_left_hand(random_index, 0, 85)
+        add_chord_left_hand(random_index, 0, 115)
 
     # Generate music
-    while right_hand.duration.quarterLength < number_of_quarters:
+    while right_hand.duration.quarterLength < quarters_count:
         add_one_interval()
 
     # Combine hands into stream
