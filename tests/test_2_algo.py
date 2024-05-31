3642fdfad52998069006262e21fd811e010c7187
sergfedchin <sergfedchin@gmail.com>
2024-05-31 12:19:11 +0300

    🧪 Add GPT-2 tests

--- tests/test_2_algo.py
+++ tests/test_2_algo.py
@@ -1,74 +1,15 @@
-import os
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
-
-class AlgoGenerationError(Exception):
-    pass
+from .utils import clear_generated_data, try_editing, try_generation
 
 
-def clear_generated_data():
-    for file in [f for f in os.listdir('generated_data') if not f.startswith('.')]:
-        os.remove(os.path.join('generated_data', file))
-
-
-def try_generation(generate_button: WebElement, wait: WebDriverWait):
-    """Try to edit a track and raise an exception if failed."""
-
-    def check_generated_files(wait_mp3_file_time: int = 40):
-        """
-        Checks whether all 4 files generated. Since wav to mp3 conversion take quite some time,
-        function check_iteration() that actually checks all 4 files is invoked in a loop with a
-        cooldown of ITERATION_TIME seconds until wait_mp3_file_time has passed (fails the test)
-        or the mp3 file has been found.
-        """
-        def check_iteration():
-            extensions = ['.mid', '.musicxml', '.pdf', '.mp3']
-            generated_files = os.listdir('generated_data')
-            for ext in extensions:
-                for f in generated_files:
-                    if f.endswith(ext):
-                        break
-                else:
-                    return False
-            return True
-
-        ITERATION_TIME = 2
-        tries = 0
-        while not check_iteration():
-            if tries == wait_mp3_file_time // ITERATION_TIME:
-                raise AlgoGenerationError("Didn't find some generated files.")
-            tries += 1
-            sleep(ITERATION_TIME)
-
-    generate_button.click()
-    wait.until(EC.element_to_be_clickable(generate_button))
-    check_generated_files()
-    sleep(2)
-
-
-def try_editing(render_button: WebElement, wait: WebDriverWait):
-    """Try to edit a track and raise an exception if failed."""
-
-    def check_edited_file():
-        """Check whether the edited mp3 file has been saved."""
-        for file in os.listdir('generated_data'):
-            if file.startswith('edited_') and file.endswith('.mp3'):
-                return
-        raise AlgoGenerationError("Track editing failed.")
-
-    render_button.click()
-    wait.until(EC.element_to_be_clickable(render_button))
-    sleep(2)
-    check_edited_file()
-    sleep(2)
+EXTENSIONS = ['.mid', '.musicxml', '.pdf', '.mp3']
 
 
 def test_algo_1(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
@@ -85,14 +26,14 @@ def test_algo_1(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
 
     wait = WebDriverWait(driver, 50)
     clear_generated_data()
-    try_generation(generate_button, wait)
+    try_generation(generate_button, wait, EXTENSIONS)
     clear_generated_data()
 
     duration_select.select_by_visible_text("00:30")
     tempo_select.select_by_visible_text("Slow")
     scale_select.select_by_visible_text("F#")
 
-    try_generation(generate_button, wait)
+    try_generation(generate_button, wait, EXTENSIONS)
     clear_generated_data()
 
 
@@ -112,7 +53,7 @@ def test_algo_2(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
     scale_select.select_by_visible_text("Eb")
 
     clear_generated_data()
-    try_generation(generate_button, WebDriverWait(driver, 50))
+    try_generation(generate_button, WebDriverWait(driver, 50), EXTENSIONS)
 
 
 def test_algo_3(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
@@ -131,7 +72,7 @@ def test_algo_3(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
     scale_select.select_by_visible_text("F#")
 
     clear_generated_data()
-    try_generation(generate_button, WebDriverWait(driver, 50))
+    try_generation(generate_button, WebDriverWait(driver, 50), EXTENSIONS)
 
 
 def test_algo_4(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
@@ -150,7 +91,7 @@ def test_algo_4(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
     scale_select.select_by_visible_text("G#")
 
     clear_generated_data()
-    try_generation(generate_button, WebDriverWait(driver, 50))
+    try_generation(generate_button, WebDriverWait(driver, 50), EXTENSIONS)
 
     edit_button = driver.find_element(by=By.ID, value="EditAlgoTrackButton")
     start_time = driver.find_element(by=By.ID, value="AlgoStartTime")
@@ -187,6 +128,6 @@ def test_algo_5(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
     scale_select.select_by_visible_text("B")
 
     clear_generated_data()
-    try_generation(generate_button, WebDriverWait(driver, 50))
+    try_generation(generate_button, WebDriverWait(driver, 50), EXTENSIONS)
     sleep(3)
     clear_generated_data()
---
+++ tests/test_4_gpt2.py
@@ -0,0 +1,92 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+from .utils import clear_generated_data, try_editing, try_generation
+
+
+EXTENSIONS = ['.mid', '.mp3']
+
+
+def test_gpt2_1(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
+    ip, port, driver = get_ip, get_port, get_driver
+
+    driver.get(f'http://{ip}:{port}/')
+    driver.find_element(by=By.ID, value="startButton").click()
+    driver.find_element(by=By.ID, value="neuroEngine").click()
+
+    generate_button = driver.find_element(by=By.ID, value="GenerateNeuralMusic")
+    model_select = Select(driver.find_element(by=By.ID, value="Model"))
+    duration_select = Select(driver.find_element(by=By.ID, value="DurationOfTheTrack"))
+    tempo_select = Select(driver.find_element(by=By.ID, value="TempoOfTheTrack"))
+
+    model_select.select_by_visible_text("GPT-2")
+    duration_select.select_by_visible_text("00:30")
+    tempo_select.select_by_visible_text("Slow")
+
+    clear_generated_data()
+    try_generation(generate_button, WebDriverWait(driver, 90), EXTENSIONS)
+
+
+def test_gpt2_2(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
+    ip, port, driver = get_ip, get_port, get_driver
+    driver.get(f'http://{ip}:{port}/generate/neural')
+
+    model_select = Select(driver.find_element(by=By.ID, value="Model"))
+    generate_button = driver.find_element(by=By.ID, value="GenerateNeuralMusic")
+    duration_select = Select(driver.find_element(by=By.ID, value="DurationOfTheTrack"))
+    tempo_select = Select(driver.find_element(by=By.ID, value="TempoOfTheTrack"))
+    composer_select = Select(driver.find_element(by=By.ID, value="Composer"))
+    correct_scale = driver.find_element(by=By.ID, value="NeuroCorrectScale")
+
+    model_select.select_by_visible_text("GPT-2")
+    duration_select.select_by_visible_text("00:30")
+    tempo_select.select_by_visible_text("Slow")
+    composer_select.select_by_visible_text("Chopin")
+    correct_scale.click()
+
+    clear_generated_data()
+    try_generation(generate_button, WebDriverWait(driver, 90), EXTENSIONS)
+
+
+def test_gpt2_3(get_ip: str, get_port: int, get_driver: webdriver.Chrome):
+    ip, port, driver = get_ip, get_port, get_driver
+    driver.get(f'http://{ip}:{port}/generate/neural')
+
+    generate_button = driver.find_element(by=By.ID, value="GenerateNeuralMusic")
+    composer_select = Select(driver.find_element(by=By.ID, value="Composer"))
+    model_select = Select(driver.find_element(by=By.ID, value="Model"))
+    duration_select = Select(driver.find_element(by=By.ID, value="DurationOfTheTrack"))
+    tempo_select = Select(driver.find_element(by=By.ID, value="TempoOfTheTrack"))
+    correct_scale = driver.find_element(by=By.ID, value="NeuroCorrectScale")
+    edit_button = driver.find_element(by=By.ID, value="EditNeuroTrackButton")
+    start_time = driver.find_element(by=By.ID, value="NeuroStartTime")
+    end_time = driver.find_element(by=By.ID, value="NeuroEndTime")
+    add_fades = driver.find_element(by=By.ID, value="NeuroAddFades")
+    fade_in = driver.find_element(by=By.ID, value="NeuroFadeInTime")
+    fade_out = driver.find_element(by=By.ID, value="NeuroFadeOutTime")
+    render_button = driver.find_element(by=By.ID, value="EditNeuroRenderButton")
+
+    model_select.select_by_visible_text("GPT-2")
+    duration_select.select_by_visible_text("00:30")
+    tempo_select.select_by_visible_text("Slow")
+    composer_select.select_by_visible_text("Bach")
+    correct_scale.click()
+
+    clear_generated_data()
+    try_generation(generate_button, WebDriverWait(driver, 90), EXTENSIONS)
+
+    edit_button.click()
+    start_time.send_keys(Keys.BACKSPACE * 2 + "05")
+    end_time.send_keys(Keys.BACKSPACE * 2 + "25")
+    add_fades.click()
+    fade_in.send_keys(Keys.BACKSPACE + "3")
+    fade_out.send_keys(Keys.BACKSPACE + "6")
+
+    try_editing(render_button, WebDriverWait(driver, 20))
+    sleep(3)
+    clear_generated_data()
